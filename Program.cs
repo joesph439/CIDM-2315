d5c84a7caacf7e85a322eb3b8acc62a95f3acf0b
joesph439 <jlbarrientez1@buffs.wtamu.edu>
2024-08-28 10:33:54 -0500

    Homework1.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Week1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int Year = 2022;
+       string Major ="CIS"
+       float stu_gpa = 3.5f;
+       double Weight = 73.5;
+       char letter_grade = "A";
+       
+       const int LabCapacity = 50;
+       Console.WriteLine(LabCapacity);
+
+    }
+}
